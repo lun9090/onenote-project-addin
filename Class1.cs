2a98ef21bbc44b36166fabc80d413d474b2ff5b4
liting <liting@DESKTOP-RSDCS44>
2021-10-14 16:09:48 +0800

    20211014提交： 1、优化获取标签逻辑，删除所有标题中标签后，更新标签信息。 2、新增未开展标签及标题更新功能，功能开发中。添加项目文件。

---
+++ Class1.cs
@@ -0,0 +1,243 @@
+﻿using Extensibility;
+using Microsoft.Office.Core;
+using Microsoft.Office.Interop.OneNote;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+using OneNote = Microsoft.Office.Interop.OneNote;
+
+namespace litingaddin
+{
+    [Guid("308DEEE4-9577-431A-8940-3C1A8418BD06"), ProgId("litingaddin.Class1")]
+    public class Class1 : IDTExtensibility2, IRibbonExtensibility
+    {
+        private OneNote.Application onApp = new OneNote.Application();
+        private object application;
+        public void OnConnection(object Application, ext_ConnectMode ConnectMode, object AddInInst, ref Array custom)
+        {
+            application = Application;
+        }
+
+        public void OnDisconnection(ext_DisconnectMode RemoveMode, ref Array custom)
+        {
+            application = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+
+        public void OnAddInsUpdate(ref Array custom)
+        {
+
+        }
+
+        public void OnStartupComplete(ref Array custom)
+        {
+
+        }
+
+        public void OnBeginShutdown(ref Array custom)
+        {
+            if (application != null)
+            {
+                application = null;
+            }
+        }
+
+        public string GetCustomUI(string RibbonID)
+        {
+            return Properties.Resources.ribbon;
+        }
+        static XNamespace ns = null;
+        public void update_tittle(IRibbonControl control)
+        {
+            OneNote.Application onenoteApp = new OneNote.Application();
+            string xml;
+            var pageid = onenoteApp.Windows.CurrentWindow.CurrentPageId;
+            onenoteApp.GetPageContent(pageid, out xml, OneNote.PageInfo.piAll);
+            var doc = XDocument.Parse(xml);
+            ns = doc.Root.Name.Namespace;
+            var outLine_title = doc.Descendants(ns + "T").FirstOrDefault();
+            //MessageBox.Show(outLine.Value);
+            //XElement element =doc.Descendants(ns + "TagDef").FirstOrDefault();
+            var title_update = string.Empty;
+            int index = outLine_title.Value.LastIndexOf('｜');
+            outLine_title.Value = outLine_title.Value.Substring(index + 1);
+            foreach (XElement tags in from node in doc.Descendants(ns +
+      "TagDef") select node)
+            {
+                var outLine_tag = tags.Attribute("name").Value.ToString();
+                if (outLine_tag == "Page Tags")
+                {
+                    break;
+                }
+                //else if(outLine_title.Value.Contains(outLine_tag)==true)
+                //{
+                //    break;
+                //}
+                //else if (outLine_title.Value.Contains(outLine_tag) == false)
+                //{
+                //    outLine_title.Value = outLine_tag + "｜" + outLine_title.Value;
+                //}
+                else
+                {
+                    outLine_title.Value = outLine_tag + "｜" + outLine_title.Value;
+                }
+            }
+            
+            onenoteApp.UpdatePageContent(doc.ToString(), System.DateTime.MinValue);
+        }
+        public void project_start(IRibbonControl control)
+        {
+            OneNote.Application onenoteApp = new OneNote.Application();
+            string xml;
+            var pageid = onenoteApp.Windows.CurrentWindow.CurrentPageId;
+            onenoteApp.GetPageContent(pageid, out xml, OneNote.PageInfo.piAll);
+            var doc = XDocument.Parse(xml);
+            ns = doc.Root.Name.Namespace;
+            var outLine_title = doc.Descendants(ns + "T").FirstOrDefault();
+            //MessageBox.Show(outLine.Value);
+            //XElement element =doc.Descendants(ns + "TagDef").FirstOrDefault();
+            var title_update = string.Empty;
+            int index = outLine_title.Value.LastIndexOf('｜');
+            outLine_title.Value = outLine_title.Value.Substring(index + 1);
+            foreach (XElement tags in from node in doc.Descendants(ns +
+      "TagDef")
+                                      select node)
+            {
+                var outLine_tag = tags.Attribute("name").Value.ToString();
+                if (outLine_tag == "Page Tags")
+                {
+                    break;
+                }
+                //else if(outLine_title.Value.Contains(outLine_tag)==true)
+                //{
+                //    break;
+                //}
+                //else if (outLine_title.Value.Contains(outLine_tag) == false)
+                //{
+                //    outLine_title.Value = outLine_tag + "｜" + outLine_title.Value;
+                //}
+                else
+                {
+                    outLine_title.Value = outLine_tag + "｜" + outLine_title.Value;
+                }
+            }
+
+            onenoteApp.UpdatePageContent(doc.ToString(), System.DateTime.MinValue);
+        }
+
+        class CCOMStreamWrapper : IStream
+        {
+            public CCOMStreamWrapper(System.IO.Stream streamWrap)
+            {
+                m_stream = streamWrap;
+            }
+
+            public void Clone(out IStream ppstm)
+            {
+                ppstm = new CCOMStreamWrapper(m_stream);
+            }
+
+            public void Commit(int grfCommitFlags)
+            {
+                m_stream.Flush();
+            }
+
+            public void CopyTo(IStream pstm, long cb, IntPtr pcbRead, IntPtr pcbWritten)
+            {
+            }
+
+            public void LockRegion(long libOffset, long cb, int dwLockType)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Read(byte[] pv, int cb, IntPtr pcbRead)
+            {
+                Marshal.WriteInt64(pcbRead, m_stream.Read(pv, 0, cb));
+            }
+
+            public void Revert()
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Seek(long dlibMove, int dwOrigin, IntPtr plibNewPosition)
+            {
+                long posMoveTo = 0;
+                Marshal.WriteInt64(plibNewPosition, m_stream.Position);
+                switch (dwOrigin)
+                {
+                    case 0:
+                        {
+                            /* STREAM_SEEK_SET */
+                            posMoveTo = dlibMove;
+                        }
+                        break;
+                    case 1:
+                        {
+                            /* STREAM_SEEK_CUR */
+                            posMoveTo = m_stream.Position + dlibMove;
+
+                        }
+                        break;
+                    case 2:
+                        {
+                            /* STREAM_SEEK_END */
+                            posMoveTo = m_stream.Length + dlibMove;
+                        }
+                        break;
+                    default:
+                        return;
+                }
+                if (posMoveTo >= 0 && posMoveTo < m_stream.Length)
+                {
+                    m_stream.Position = posMoveTo;
+                    Marshal.WriteInt64(plibNewPosition, m_stream.Position);
+                }
+            }
+
+            public void SetSize(long libNewSize)
+            {
+                m_stream.SetLength(libNewSize);
+            }
+
+            public void Stat(out System.Runtime.InteropServices.ComTypes.STATSTG pstatstg, int grfStatFlag)
+            {
+                pstatstg = new System.Runtime.InteropServices.ComTypes.STATSTG();
+                pstatstg.cbSize = m_stream.Length;
+                if ((grfStatFlag & 0x0001/* STATFLAG_NONAME */) != 0)
+                    return;
+                pstatstg.pwcsName = m_stream.ToString();
+            }
+
+            public void UnlockRegion(long libOffset, long cb, int dwLockType)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public void Write(byte[] pv, int cb, IntPtr pcbWritten)
+            {
+                Marshal.WriteInt64(pcbWritten, 0);
+                m_stream.Write(pv, 0, cb);
+                Marshal.WriteInt64(pcbWritten, cb);
+            }
+
+            private System.IO.Stream m_stream;
+        }
+        public IStream GetImage(string imageName)
+        {
+            MemoryStream mem = new MemoryStream();
+            Properties.Resources.HelloWorld.Save(mem, ImageFormat.Png);
+            return new CCOMStreamWrapper(mem);
+        }
+
+
+    }
+}
